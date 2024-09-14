ba6ba05aa9cd79f853c946867ed5f34562aa07a1
rally.lin@duke.edu <rl326@duke.edu>
2024-09-14 18:28:25 -0400

    navigation

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/screens/Login';
+import Dashboard from './src/screens/Dashboard';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer initialRouteName="Login">
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
