f1b5648139425f19b6631f3ebc5dc36ccd91c21a
rally.lin@duke.edu <rl326@duke.edu>
2024-09-15 03:17:35 -0400

    Buggy authentication

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // Adding plugins for handling class properties, private methods, and private properties
+    ['@babel/plugin-transform-class-properties', { loose: true }], 
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+
+    // Plugin for environment variables with react-native-dotenv
+    ['module:react-native-dotenv', {
+      moduleName: '@env',  // Import variables as '@env'
+      path: '.env',        // Ensure the correct path to your .env file
+      allowUndefined: true, // Avoid errors if variables are undefined
+    }],
+  ],
 };
