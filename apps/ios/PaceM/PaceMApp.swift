eb8025673bec8ac49e6f88dab84115bf009c4a42
rally.lin@duke.edu <linrally@gmail.com>
2025-07-26 23:55:24 -0400

    add ios app

---
+++ apps/ios/PaceM/PaceMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceMApp.swift
+//  PaceM
+//
+//  Created by Rally Lin on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct PaceMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/ios/PaceMTests/PaceMTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaceMTests.swift
+//  PaceMTests
+//
+//  Created by Rally Lin on 7/26/25.
+//
+
+import Testing
+@testable import PaceM
+
+struct PaceMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
